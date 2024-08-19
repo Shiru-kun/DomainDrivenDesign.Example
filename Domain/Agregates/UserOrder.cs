c5e2539d5da72f0691d859780190a28c26d980c8
Focas Kandulo <focas.kandulo@ambisig.com>
2024-08-19 11:30:30 +0200

    Add project files.

---
+++ Domain/Agregates/UserOrder.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Agregates
+{
+    public class UserOrder
+    {
+        public Guid Id { get; set; }
+        public User User { get; set; }  
+        public Order Order { get; set; }  
+
+    }
+}
